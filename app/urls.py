3595783491d175c63987d74e66167e2718fe9536
Dimash <dbazarkhanov33@gmail.com>
2023-04-30 11:28:47 +0600

    upd

--- app/urls.py
+++ app/urls.py
@@ -5,11 +5,11 @@ app_name = 'app'
 
 urlpatterns = [
     path('currencies/', CurrencyList.as_view(), name='currency-list'),
-    path('currencies/<int:id>/', CurrencyDetail.as_view(), name='currency-detail'),
+    # path('currencies/<int:id>/', CurrencyDetail.as_view(), name='currency-detail'),
     path('balances/', BalanceList.as_view(), name='balance-list'),
-    path('balances/<int:id>/', BalanceDetail.as_view(), name='balance-detail'),
+    # path('balances/<int:id>/', BalanceDetail.as_view(), name='balance-detail'),
     path('transactions/', TransactionList.as_view(), name='transaction-list'),
-    path('transactions/<int:id>/', TransactionDetail.as_view(), name='transaction-detail'),
+    # path('transactions/<int:id>/', TransactionDetail.as_view(), name='transaction-detail'),
     path('polls/', PollList.as_view(), name='index'),
-    path('polls/<int:id>/', PollDetail.as_view(), name='poll-detail'),
+    # path('polls/<int:id>/', PollDetail.as_view(), name='poll-detail'),
 ]
