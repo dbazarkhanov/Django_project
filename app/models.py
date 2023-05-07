e0149366ee332addbea9c84dddaac732f4893234
Dimash <dbazarkhanov33@gmail.com>
2023-05-07 22:41:33 +0600

    upd

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ import json
 from app.secrets import API_KEY
 import sys
 sys.path.append('/Users/Dell/Desktop/KBTU/django/project/user')
-from user .models import User
+from user.models import User
 from django.db import models
 
 # Create your models here.
@@ -13,10 +13,10 @@ from django.db import models
 class Currency(models.Model):
     name = models.CharField(null=True, max_length=100)
     symbol = models.CharField(null=True, max_length=100)
-    image = models.ImageField(upload_to='media/currency/', max_length=254, default='')
+    image = models.ImageField(upload_to='media/', max_length=254, default='')
     price = models.FloatField(default=0)
 
-
+'''
 class Balance(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='balance')
     balance = models.FloatField(default=0)
@@ -26,29 +26,44 @@ class Balance(models.Model):
 
     def __str__(self):
         return f"{self.user}"
+'''
+
+#A single element inside person's wallet
+class WalletElement(models.Model):
+    currency = models.ForeignKey(to=Currency, on_delete=models.CASCADE,)
+    quantity = models.FloatField(null=True)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE,)
+
+    def __str__(self):
+        return f"Элемент кошелька {self.user.username}, валюта {self.currency.name}"
+
+
+
+    
 
 #Stores historical data (all transactions of all users)
 class Transaction(models.Model):
-    type = models.CharField(max_length=10, null=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='user')
-    to = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='to')
-    currency_id = models.ForeignKey(to=Currency, on_delete=models.CASCADE,)
+    seller = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='user')
+    buyer = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='to')
+    currency = models.ForeignKey(to=Currency, on_delete=models.CASCADE,)
     quantity = models.FloatField(default=0)
+    price = models.FloatField(default=0)
 
     def sum(self):
-        return self.currency_id.price * self.quantity
+        return self.price * self.quantity
 
 class Poll(models.Model):
     user = models.ForeignKey(to=User, on_delete=models.CASCADE)
     currency = models.ForeignKey(to=Currency, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=0)
+    price = models.FloatField(default=0)
     created_timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"Объявление о продаже {self.user.username}"
 
     def sum(self):
-        return self.currency.price * self.quantity
+        return self.price * self.quantity
     
 class CMC(models.Model):
     def __init__(self, token):
--- app/urls.py
+++ app/urls.py
@@ -2,11 +2,15 @@ from django.urls import path
 from django.views.generic import TemplateView
 from .views import *
 
+
 app_name = 'app'
 
 urlpatterns = [
-    path('main/', TemplateView.as_view(template_name='main.html'), name='main'),
-    path('offers/', PollList.as_view(), name='offers'),
-    path('currencies/', CurrencyList.as_view(), name='currency_list'),
+    path('', TemplateView.as_view(template_name='main.html'), name='main'),
+    path('currencies/', CurrencyList.as_view(), name='currency-list'),
+    # path('currencies/<int:id>/', CurrencyDetail.as_view(), name='currency-detail'),
     path('transactions/', TransactionList.as_view(), name='transaction-list'),
+    # path('transactions/<int:id>/', TransactionDetail.as_view(), name='transaction-detail'),
+    path('polls/', PollList.as_view(), name='offers'),
+    # path('polls/<int:id>/', PollDetail.as_view(), name='poll-detail'),
 ]
